cb310d7c158ad4127a437b52550fa44207695753
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2024-08-27 14:40:11 +0330

    Feat: Add i18n to angular-website

--- apps/angular-website/src/app/app.component.ts
+++ apps/angular-website/src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar.component';
+import localeEn from '@angular/common/locales/en';
+import localeFa from '@angular/common/locales/fa';
+import { Location, registerLocaleData } from '@angular/common';
 
+// Register locales
+registerLocaleData(localeFa, 'fa');
+registerLocaleData(localeEn, 'en');
 @Component({
   standalone: true,
   imports: [RouterModule, NavBarComponent],
@@ -11,4 +17,12 @@ import { NavBarComponent } from './components/nav-bar.component';
 })
 export class AppComponent {
   title = 'angular-website';
+  constructor(private location: Location) {}
+
+  switchLanguage(lang: string) {
+    // Change the language by updating the path
+    const url = this.location.path().replace(/\/(en|fa)/, `/${lang}`);
+    window.location.href = url;
+  }
+
 }
--- apps/angular-website/src/app/app.routes.ts
+++ apps/angular-website/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  // { path: ':lang', component: HomeComponent }, // Handles routes with language code
+  // { path: '', redirectTo: '/en', pathMatch: 'full' } // Default to English
+];
