b2323489d961f8d3f491edef754ac3c65c8f64d4
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2024-09-04 17:36:03 +0330

    Feat: Add table to users-list component in angular-dashboard

--- libs/angular-dashboard/users-list/src/lib/users-list/users-list.component.ts
+++ libs/angular-dashboard/users-list/src/lib/users-list/users-list.component.ts
@@ -5,15 +5,17 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersService } from '../data-access/users.service';
+import { FormsModule } from '@angular/forms';
 
 interface User {
   username: string;
+  createdDate: string;
 }
 
 @Component({
   selector: 'lib-users-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   providers: [UsersService],
   templateUrl: './users-list.component.html',
   encapsulation: ViewEncapsulation.Emulated,
@@ -22,6 +24,12 @@ interface User {
 export class UsersListComponent implements OnInit {
   usersService = inject(UsersService);
   users: User[] = [];
+  paginatedUsers: any[] = [];
+  searchTerm: string = '';
+  filterDate: string = '';
+  currentPage: number = 1;
+  itemsPerPage: number = 2;
+  totalPages: number = 1;
 
   ngOnInit() {
     this.getUsers();
@@ -31,7 +39,7 @@ export class UsersListComponent implements OnInit {
     this.usersService.getUsers().subscribe({
       next: (res) => {
         this.users = res.users;
-        console.log('users', this.users);
+        this.applyFilters();
       },
       error: (err) => {
         console.log('err', err);
@@ -39,14 +47,66 @@ export class UsersListComponent implements OnInit {
     });
   }
 
+  applyFilters(): void {
+    let filteredUsers = this.users;
+
+    if (this.searchTerm) {
+      filteredUsers = filteredUsers.filter((user) =>
+        user.username.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
+
+    if (this.filterDate) {
+      // Assuming users have a "createdDate" field in ISO format
+      filteredUsers = filteredUsers.filter((user) =>
+          user.createdDate.startsWith(this.filterDate)
+        // user.createdDate = this.filterDate
+      );
+    }
+
+    this.totalPages = Math.ceil(filteredUsers.length / this.itemsPerPage);
+    this.paginateUsers(filteredUsers);
+  }
+
+  paginateUsers(filteredUsers: any[]): void {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.paginatedUsers = filteredUsers.slice(start, end);
+  }
+
+  onSearch(): void {
+    this.currentPage = 1;
+    this.applyFilters();
+  }
+
+  onFilterDate(): void {
+    this.currentPage = 1;
+    this.applyFilters();
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.applyFilters();
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.applyFilters();
+    }
+  }
+
   deleteUser(username: string): void {
     this.usersService.deleteUser(username).subscribe({
       next: () => {
         this.users = this.users.filter(user => user.username !== username);
+        this.applyFilters();
       },
       error: (error) => {
         console.error('Error deleting user:', error);
-      }
+      },
     });
   }
 }
